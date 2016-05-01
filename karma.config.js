57d7e020ec61455b93efd857f5ba69239085c371
Brian Kim <briankimpossible@gmail.com>
2016-05-01 01:25:40 -0400

    add karma sourcemaps

--- karma.config.js
+++ karma.config.js
@@ -23,8 +23,8 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-      'tests/**/*spec.js': ['webpack'],
-      'src/**/*.js': ['webpack'],
+      'tests/**/*spec.js': ['webpack', 'sourcemap'],
+      'src/**/*.js': ['webpack', 'sourcemap'],
     },
 
     // test results reporter to use
