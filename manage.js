0758cd78afadcb080ac8fd7dcabb6d8f7f3e049e
Brian Kim <briankimpossible@gmail.com>
2016-06-08 01:09:37 -0400

    get out of here exploratory code I gotta code

--- manage.js
+++ manage.js
@@ -1,38 +1,8 @@
 #! /usr/bin/env node
+require('babel-register');
 var path = require('path');
-var SystemJS = require('systemjs');
 
-System.config({
-  transpiler: 'plugin-babel',
-  packageConfigPaths: ['node_modules/*/package.json'],
-  paths: {
-    '*': 'node_modules/*',
-  },
-  map: {
-    'plugin-babel': 'systemjs-plugin-babel/plugin-babel.js',
-    'systemjs-babel-build': 'systemjs-plugin-babel/systemjs-babel-node.js',
-  },
-  meta: {
-    '*.js': {
-      babelOptions: {
-        "presets": [
-          'babel-preset-react/index.js',
-          // "es2015",
-          // "react",
-          // "stage-2",
-        ],
-        "plugins": [
-          // "transform-runtime"
-        ]
-      },
-    },
-    '*.json': { loader: 'json-plugin' },
-  },
-});
-
-System.import(path.join(process.cwd(), process.argv[2])).then((module) => {
-  console.log(module());
-});
+require(path.join(process.cwd(), process.argv[2]));
 
 process.on('unhandledRejection', function(error, p) {
   console.error(error.stack || error);
